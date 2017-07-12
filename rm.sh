09a461d5827a8caa06bec062f762dad6cdbaad01
Marco Couto <marcocouto90@gmail.com>
2017-07-12 23:56:40 +0100

    ...

---
+++ rm.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+sleep 3
+adb shell pm uninstall com.Orlando.opensource.bikeorlando
+sleep 3
+adb shell pm uninstall com.bigfootsoftwares.note
+sleep 3
+adb shell pm uninstall com.cardinalsolutions.loginpad
+sleep 3
+adb shell pm uninstall com.com.abpconsult.twowaygallery
+sleep 3
+adb shell pm uninstall com.danielkao.autoscreenonoff
+sleep 3
+adb shell pm uninstall com.doodeec.filmster
+sleep 3
+adb shell pm uninstall com.esri.runtime.android.localgeofence
+sleep 3
+adb shell pm uninstall com.example.android.actionbarcompat.basic
+sleep 3
+adb shell pm uninstall com.example.android.actionbarcompat.listpopupmenu
+sleep 3
+adb shell pm uninstall com.example.android.actionbarcompat.shareactionprovider
+sleep 3
+adb shell pm uninstall com.example.android.actionbarcompat.styled
+sleep 3
+adb shell pm uninstall com.example.android.activityscenetransitionbasic
+sleep 3
+adb shell pm uninstall com.example.android.animationdemo
+sleep 3
+adb shell pm uninstall com.example.android.basicaccessibility
+sleep 3
+adb shell pm uninstall com.example.android.basicandroidkeystore
+sleep 3
+adb shell pm uninstall com.example.android.basiccontactables
+sleep 3
+adb shell pm uninstall com.example.android.basicnetworking
+sleep 3
+adb shell pm uninstall com.example.android.basicnotifications
+sleep 3
+adb shell pm uninstall com.example.android.basicsyncadapter
+sleep 3
+adb shell pm uninstall com.example.android.camera2basic
+sleep 3
+adb shell pm uninstall com.example.android.cardemulation
+sleep 3
+adb shell pm uninstall com.example.android.cardreader
+sleep 3
+adb shell pm uninstall com.example.android.cardview
+sleep 3
+adb shell pm uninstall com.example.android.clippingbasic
+sleep 3
+adb shell pm uninstall com.example.android.displayingbitmaps
+sleep 3
+adb shell pm uninstall com.example.android.donebar
+sleep 3
+adb shell pm uninstall com.example.android.elevationbasic
+sleep 3
+adb shell pm uninstall com.example.android.elevationdrag
+sleep 3
+adb shell pm uninstall com.example.android.floatingactionbuttonbasic
+sleep 3
+adb shell pm uninstall com.example.android.horizontalpaging
+sleep 3
+adb shell pm uninstall com.example.android.immersivemode
+sleep 3
+adb shell pm uninstall com.example.android.interpolator
+sleep 3
+adb shell pm uninstall com.example.android.jobscheduler
+sleep 3
+adb shell pm uninstall com.example.android.lnotifications
+sleep 3
+adb shell pm uninstall com.example.android.navigationdrawer
+sleep 3
+adb shell pm uninstall com.example.android.networkconnect
+sleep 3
+adb shell pm uninstall com.example.android.pdfrendererbasic
+sleep 3
+adb shell pm uninstall com.example.android.revealeffectbasic
+sleep 3
+adb shell pm uninstall com.example.android.slidingtabsbasic
+sleep 3
+adb shell pm uninstall com.example.android.slidingtabscolors
+sleep 3
+adb shell pm uninstall com.example.android.storageclient
+sleep 3
+adb shell pm uninstall com.example.android.swiperefreshlayoutbasic
+sleep 3
+adb shell pm uninstall com.example.android.swiperefreshmultipleviews
+sleep 3
+adb shell pm uninstall com.example.android.textlinkify
+sleep 3
+adb shell pm uninstall com.example.myapplication.app
+sleep 3
+adb shell pm uninstall com.example.onedrivesdk.saversample
+sleep 3
+adb shell pm uninstall com.example.shivam.apphub
+sleep 3
+adb shell pm uninstall com.example.shivam.ixigo
+sleep 3
+adb shell pm uninstall com.greenlab.trepnlib
+sleep 3
+adb shell pm uninstall com.jerrellmardis.amphitheatre.dev
+sleep 3
+adb shell pm uninstall com.jesusm.textfielderror
+sleep 3
+adb shell pm uninstall com.mariogrip.octoprint
+sleep 3
+adb shell pm uninstall com.specyci.mirrorwechat
+sleep 3
+adb shell pm uninstall com.survivingwithandroid.charts
+sleep 3
+adb shell pm uninstall com.swarmnyc.fragmenttransitions
+sleep 3
+adb shell pm uninstall com.urucas.ptremotecontrol
+sleep 3
+adb shell pm uninstall de.vogella.android.listactivity
+sleep 3
+adb shell pm uninstall delta.instagram
+sleep 3
+adb shell pm uninstall fooving.cc.androiduidev
+sleep 3
+adb shell pm uninstall jp.mixi.sample.controller
+sleep 3
+adb shell pm uninstall jp.muo.obbsample
+sleep 3
+adb shell pm uninstall net.ahammad.showedittextpassword
+sleep 3
+adb shell pm uninstall net.buildbox.pokeri.layout_linearlayout
+sleep 3
+adb shell pm uninstall nl.changer.audiowife
+sleep 3
+adb shell pm uninstall org.developfreedom.logmein
+sleep 3
+adb shell pm uninstall test.gxy.com.androidltest
