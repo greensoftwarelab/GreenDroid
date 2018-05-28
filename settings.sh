8fdaeb1a77fe0364e1370a706740e4f9a5f0757c
Rui Rua <rui.rrua@gmail.com>
2018-05-28 17:53:24 +0100

    settings

--- settings.sh
+++ settings.sh
@@ -19,12 +19,16 @@ function b_echo {
 
 function getAndroidState(){
 	used_cpu=$(adb shell dumpsys cpuinfo | grep  "Load" | cut -f2 -d\ )
-	free_mem=$(adb shell dumpsys meminfo | grep "Free RAM.*" | cut -f2 -d: | cut -f1 -d\( | tr -d ' ')
-	nprocesses=$(adb shell top -n 1 | grep -v ".*root" | grep -v ".*system" | wc -l) #take the K/M and -4
-	nr_procceses=$(($nprocesses -4))
+	free_mem=$(adb shell dumpsys meminfo | grep "Free RAM.*" | cut -f2 -d: | cut -f1 -d\( | tr -d ' '| sed "s/K//g" | sed "s/,//g")
+	nprocesses=$(adb shell top -n 1 | grep -v "root" | grep -v "system" | wc -l) #take the K/M and -4
+	nr_procceses=$(($nprocesses -5))
+	sdk_level=$(adb shell getprop ro.build.version.release)
+	api_level=$(adb shell getprop ro.build.version.sdk )
 	eval "$1='$used_cpu'"
 	eval "$2='$free_mem'"
 	eval "$3='$nr_procceses'"
+	eval "$4='$sdk_level'"
+	eval "$5='$api_level'"
 }
 
 function getSO(){
